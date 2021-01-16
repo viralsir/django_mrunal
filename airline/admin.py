3ca2f432e4bc9172ecadf4116831e48505a06d7d
viralsir <viralsir2018@gmail.com>
2021-01-16 17:34:09 +0530

    airport and flights models are updated and relationship mentioned.

--- airline/admin.py
+++ airline/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import flights,airport
 # Register your models here.
+admin.site.register(flights)
+admin.site.register(airport)
--- airline/models.py
+++ airline/models.py
@@ -1,14 +1,21 @@
 '''
   makemigrations
-  migrate
+  migrate50
 
 '''
 from django.db import models
 
+class airport(models.Model):
+    city=models.CharField(max_length=60)
+    code=models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Airport({self.city},{self.code})"
+
 # Create your models here.
 class flights(models.Model):
-    origin=models.CharField(max_length=50)
-    destions=models.CharField(max_length=50)
+    origin=models.ForeignKey('airport',on_delete=models.CASCADE,related_name='arrival')
+    destions=models.ForeignKey('airport',on_delete=models.CASCADE,related_name='departure')
     durations=models.IntegerField()
 
     def __str__(self):
